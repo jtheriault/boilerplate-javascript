41d7ba5db47cb45b53ffd2ee92a9414aac351ce4
Joe Theriault <joseph@yankoismedia.com>
2015-10-19 20:01:48 -0400

    Upgrade code-copter

--- spec/quality.spec.js
+++ spec/quality.spec.js
@@ -1,4 +1,11 @@
 'use strict';
-var followsGuidelines = require('code-copter');
+var followsGuidelines = require('code-copter'),
+    guide = followsGuidelines.analyzers;
+
+followsGuidelines.configure({
+    jscs: guide.jscs,
+    jshint: guide.jshint,
+    peers: guide.requirePeers
+});
 
 describe('Code quality', followsGuidelines);
